903dafec43e350e9fa1613220c46b89400d8eba2
JosephAlieme <103365427+JosephAlieme@users.noreply.github.com>
2024-02-13 19:24:45 +0100

    Add files via upload
    Completed using SQL Server Management Studio (SSMS)

---
+++ Data Cleaning Queries.sql
@@ -0,0 +1,227 @@
+/*
+
+Data Cleaning using SQL Queries
+
+*/
+
+USE PortfolioProject
+
+Select *
+From NashvilleHousing
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- Standardizing Date Format from DateTime to Date
+
+Select saleDate, SaleDateConverted, CONVERT(Date,SaleDate)
+From NashvilleHousing
+
+
+Update NashvilleHousing
+SET SaleDate = CONVERT(Date, SaleDate)
+
+-- If it doesn't Update properly, we can use the option below:
+
+ALTER TABLE NashvilleHousing
+Add SaleDateConverted Date;
+
+Update NashvilleHousing
+SET SaleDateConverted = CONVERT(Date,SaleDate)
+
+
+ --------------------------------------------------------------------------------------------------------------------------
+
+-- Populate Property Address data
+
+Select *
+From NashvilleHousing
+Where PropertyAddress is null
+order by ParcelID
+
+--Introducing self join to fill null values in Property Address column
+Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress,b.PropertyAddress)
+From NashvilleHousing a
+JOIN NashvilleHousing b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress is null
+
+
+Update a
+SET PropertyAddress = ISNULL(a.PropertyAddress,b.PropertyAddress)
+From PortfolioProject.dbo.NashvilleHousing a
+JOIN PortfolioProject.dbo.NashvilleHousing b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress is null
+
+
+
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- Breaking out Address into Individual Columns (Address, City, State)
+
+
+Select PropertyAddress
+From NashvilleHousing
+--Where PropertyAddress is null
+--order by ParcelID
+
+SELECT
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1) as Address
+, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress)) as City
+From NashvilleHousing
+
+
+ALTER TABLE NashvilleHousing
+Add PropertySplitAddress Nvarchar(255);
+
+Update NashvilleHousing
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 )
+
+
+ALTER TABLE NashvilleHousing
+Add PropertySplitCity Nvarchar(255);
+
+Update NashvilleHousing
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress))
+
+
+
+Select *
+From NashvilleHousing
+
+
+-- Breaking owner address using another method (PARSENAME)
+Select OwnerAddress
+From NashvilleHousing
+
+
+Select
+PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3),
+PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2),
+PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
+From NashvilleHousing
+
+
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitAddress Nvarchar(255);
+
+Update NashvilleHousing
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
+
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitCity Nvarchar(255);
+
+Update NashvilleHousing
+SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
+
+
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitState Nvarchar(255);
+
+Update NashvilleHousing
+SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
+
+
+--To view final result in NashvilleHousing table
+Select *
+From PortfolioProject.dbo.NashvilleHousing
+
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- Changing Y and N to Yes and No in "Sold as Vacant" field using CASE Statement
+
+
+Select Distinct(SoldAsVacant), Count(SoldAsVacant)
+From PortfolioProject.dbo.NashvilleHousing
+Group by SoldAsVacant
+order by 2
+
+Select SoldAsVacant
+, CASE When SoldAsVacant = 'Y' THEN 'Yes'
+	   When SoldAsVacant = 'N' THEN 'No'
+	   ELSE SoldAsVacant
+	   END
+From NashvilleHousing
+
+
+Update NashvilleHousing
+SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
+	   When SoldAsVacant = 'N' THEN 'No'
+	   ELSE SoldAsVacant
+	   END
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Removing Duplicates using ROW_NUMBER Window function and CTE
+
+WITH RowNumCTE AS(
+Select *,
+	ROW_NUMBER() OVER (
+	PARTITION BY ParcelID,
+				 PropertyAddress,
+				 SalePrice,
+				 SaleDate,
+				 LegalReference
+				 ORDER BY
+					UniqueID
+					) row_num
+
+From NashvilleHousing
+--order by ParcelID
+)
+Select *
+From RowNumCTE
+Where row_num > 1
+Order by PropertyAddress
+
+-- This returns duplicate records available within the dataset which can then be deleted using DELETE Statement. See below
+
+WITH RowNumCTE AS(
+Select *,
+	ROW_NUMBER() OVER (
+	PARTITION BY ParcelID,
+				 PropertyAddress,
+				 SalePrice,
+				 SaleDate,
+				 LegalReference
+				 ORDER BY
+					UniqueID
+					) row_num
+
+From NashvilleHousing
+--order by ParcelID
+)
+DELETE
+From RowNumCTE
+Where row_num > 1
+
+/* 
+Note that deleting records from raw imported data should be done with thorough understanding of the database you are working 
+with as useful information might be lost. It is therefore advisable to use other options like deleting temp tables 
+or views in some cases
+*/
+
+
+---------------------------------------------------------------------------------------------------------
+
+-- Deleting Unused Columns
+
+Select *
+From NashvilleHousing
+
+
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
+DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress, SaleDate
+
+/* This permanently removes unused or irrelevant columnns from the table, and any data stored in that column will be lost
+so it is important to be careful when using DROP COLUMN in a production environment, and also to ensure that there is
+a backup of the data before making such changes
+*/
